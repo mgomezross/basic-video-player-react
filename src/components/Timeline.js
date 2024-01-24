0da813a1956ab6b71d5aa1bdd9c698682ea2ac8d
Marcos D. Gomez <mgomez@rossvideo.com>
2024-01-24 13:43:14 -0500

    adds timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -9,21 +9,21 @@ const Timeline = () => {
 
   const handleDragStart = useCallback((event) => {
     setIsDragging(true);
-    // Record the initial position of the mouse relative to the segment
+   
     const segmentRect = segmentRef.current.getBoundingClientRect();
     segmentRef.current.offsetX = event.clientX - segmentRect.left;
   }, []);
 
   const handleDragMove = useCallback((event) => {
     if (isDragging) {
-      // Calculate the new position of the segment
+
       const containerRect = containerRef.current.getBoundingClientRect();
       let newLeft = event.clientX - containerRect.left - segmentRef.current.offsetX;
 
-      // Constrain the segment within the container
+     
       newLeft = Math.max(0, Math.min(newLeft, containerRect.width - segmentRef.current.offsetWidth));
 
-      // Update the position of the segment
+ 
       segmentRef.current.style.left = newLeft + 'px';
     }
   }, [isDragging]);
@@ -64,10 +64,3 @@ const Segment = styled.div`
   position: absolute; 
   cursor: pointer; 
 `;
-<div>
-    <channel>
-    </channel>
-    <div >
-
-    </div>
-</div>
